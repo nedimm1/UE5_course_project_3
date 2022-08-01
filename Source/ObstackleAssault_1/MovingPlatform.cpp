c9fa8bc945ffc281382c82c3db8368b81a05793a
github_username <nedimmuminovic61@gmail.com>
2022-08-01 16:12:38 +0200

    adding local variables to theclass

--- Source/ObstackleAssault_1/MovingPlatform.cpp
+++ Source/ObstackleAssault_1/MovingPlatform.cpp
@@ -24,9 +24,13 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FVector LocalVector = MyVector;
+
+	LocalVector.Z = LocalVector.Z + 100;
+
 	MyVector.Y = MyVector.Y + 1;
 
-	SetActorLocation(MyVector);
+	SetActorLocation(LocalVector);
 
 }
 
