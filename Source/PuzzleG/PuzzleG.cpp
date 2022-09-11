ab6c4b24849557e2f1fed7f899366071fe65bb3a
github_username <nedimmuminovic61@gmail.com>
2022-09-11 14:20:19 +0200

    new commits

---
+++ Source/PuzzleG/PuzzleG.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PuzzleG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PuzzleG, "PuzzleG" );
+ 
---
+++ Source/PuzzleG/PuzzleGGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PuzzleGGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APuzzleGGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APuzzleGGameMode();
+};
+
+
+
