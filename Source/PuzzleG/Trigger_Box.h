4c10697684b88acad2bd7a1176ded88530ff41bd
github_username <nedimmuminovic61@gmail.com>
2022-10-03 12:47:05 +0200

    making a new c++ component

---
+++ Source/PuzzleG/Trigger_Box.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "Trigger_Box.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEG_API UTrigger_Box : public UBoxComponent
+{
+	GENERATED_BODY()
+	
+};
